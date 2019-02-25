739f269d2ed8b862881f90dffcbe3596724b1f78
Gainsboroow <32876469+Gainsboroow@users.noreply.github.com>
2019-02-25 11:54:53 +0100

    Update Floyd_Warshall.cpp

--- Shortest Path Algorithms/Floyd_Warshall.cpp
+++ Shortest Path Algorithms/Floyd_Warshall.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 const int MAX_NODE = 1000;
-int minDistance[MAX_NODE][MAX_NODE]; // minDistance[i][j] corresponds to the mininmum distance between i and j
+int dist[MAX_NODE][MAX_NODE]; // dist[i][j] corresponds to the mininmum distance between i and j
 
 int main(){
     int nbNodes, nbEdges;
@@ -10,29 +10,29 @@ int main(){
 
     for (int node1 = 0; node1 < nbNodes; node1 ++){
         for (int node2 = 0; node2 < nbNodes; node2 ++){
-            minDistance[node1][node2] = 1e9; 
+            dist[node1][node2] = 1e9; 
         }
-        minDistance[node1][node1] = 0; // Setting the distance between a node and itself to 0
+        dist[node1][node1] = 0; // Setting the distance between a node and itself to 0
     }
 
     for (int edge = 0; edge < nbEdges; edge++){
-        int node1, node2, dist;
-        cin >> node1 >> node2 >> dist;
-        minDistance[node1][node2] = dist;
-        minDistance[node2][node1] = dist;
+        int node1, node2, _dist;
+        cin >> node1 >> node2 >> _dist;
+        dist[node1][node2] = _dist;
+        dist[node2][node1] = _dist;
     }
 
     for (int interNode = 0; interNode < nbNodes; interNode++){
         for (int node1 = 0; node1 < nbNodes; node1 ++){
             for (int node2 = 0; node2 < nbNodes; node2 ++){
-                minDistance[node1][node2] = min(minDistance[node1][node2], minDistance[node1][interNode] + minDistance[interNode][node2] );
+                dist[node1][node2] = min(dist[node1][node2], dist[node1][interNode] + dist[interNode][node2] );
             }
         }
     }
     
     for (int node1 = 0; node1 < nbNodes; node1 ++){
         for (int node2 = 0; node2 < nbNodes; node2 ++){
-            cout << minDistance[node1][node2] << ' ';
+            cout << dist[node1][node2] << ' ';
         }
         cout << '\n';
     }
